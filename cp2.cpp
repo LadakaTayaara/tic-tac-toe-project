a9d1f696b252af31e131f3a9ea6698cd52e3bda3
ladaka.tayaara <abhiventor@gmail.com>
2025-04-02 15:27:01 +0530

    gay porn v2

--- cp2.cpp
+++ cp2.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     for (int i=0;i<70;i++){
         for(int j=0;j<70;j++){
-            cout<<"gay porn";
+            cout<<"gay porn ";
         }
         cout<<"\n";
     }
